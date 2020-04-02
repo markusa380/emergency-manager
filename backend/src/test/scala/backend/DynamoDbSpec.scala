c2ba8143ce885ab0fff1923f7f677ab9f694b372
Markus Appel <markusappel@hotmail.de>
2020-04-02 22:06:41 +0200

    Fix unit tests

--- backend/src/test/scala/backend/DynamoDbSpec.scala
+++ backend/src/test/scala/backend/DynamoDbSpec.scala
@@ -6,6 +6,8 @@ import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should._
 
+import cats.effect.IO
+
 import software.amazon.awssdk.regions.Region
 import java.{util => ju}
 
@@ -13,18 +15,20 @@ import emergencymanager.commons.data.Supplies
 
 class DynamoDbSpec extends AnyFlatSpec with Matchers {
 
+  implicit val region = Region.EU_CENTRAL_1
+
   "The construction of a DynamoDb instance using the Supplies case-class" should "compile" in {
-    """val db: DynamoDb[Supplies] = DynamoDb(Region.EU_CENTRAL_1, "Foo")""" should compile
+    """val db: DynamoDb[IO, Supplies] = DynamoDb.io("Foo")""" should compile
   }
 
   "The construction of a DynamoDb instance using a complicated case class" should "compile" in {
     case class Bar(str: String)
     case class Foo(str: String, num: BigDecimal, opt: Option[String], list: List[String], item: Bar, optItem: Option[Bar])
-    """val db: DynamoDb[Foo] = DynamoDb(Region.EU_CENTRAL_1, "Foo")""" should compile
+    """val db: DynamoDb[IO, Foo] = DynamoDb.io("Foo")""" should compile
   }
 
   "The construction of a DynamoDb instance using a case class that contains an unknown type" should "not typecheck" in {
     case class Foo(date: ju.Date)
-    """val db: DynamoDb[Foo] = DynamoDb(Region.EU_CENTRAL_1, "Foo")""" shouldNot typeCheck
+    """val db: DynamoDb[IO, Foo] = DynamoDb.io("Foo")""" shouldNot typeCheck
   }
 }
