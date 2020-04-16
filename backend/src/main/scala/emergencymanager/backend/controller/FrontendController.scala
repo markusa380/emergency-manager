5c8c731d70478eb5446f97ee21df20aa5876f0af
Markus Appel <markusappel@hotmail.de>
2020-04-16 23:22:58 +0200

    Add favicon, improve page title

--- backend/src/main/scala/emergencymanager/backend/controller/FrontendController.scala
+++ backend/src/main/scala/emergencymanager/backend/controller/FrontendController.scala
@@ -8,26 +8,38 @@ import org.http4s.headers._
 import org.http4s.MediaType
 
 import java.io.File
+import java.nio.file._
 
 object FrontendController {
 
     def httpRoutes(assetsDir: String): HttpRoutes[IO] = HttpRoutes.of[IO] {
 
         case GET -> Root => handleInternalError(
-            load(assetsDir, "index.html").flatMap(
+            loadText(assetsDir, "index.html").flatMap(
                 data => Ok(data, `Content-Type`(MediaType.text.html))
             )
         )
 
         case GET -> Root / "js" / file => handleInternalError(
-            load(assetsDir, s"js/$file").flatMap(
+            loadText(assetsDir, s"js/$file").flatMap(
                 data => Ok(data, `Content-Type`(MediaType.application.javascript))
+            )
+        )
+
+        case GET -> Root / "favicon.ico" => handleInternalError(
+            loadBinary(assetsDir, s"favicon.ico").flatMap(
+                data => Ok(data, `Content-Type`(MediaType.image.`vnd.microsoft.icon`))
             )
         )
     }
 
-    def load(assetsDir: String, file: String): IO[String] = IO {
+    def loadText(assetsDir: String, file: String): IO[String] = IO {
         val path = assetsDir + File.separator + file
         scala.io.Source.fromFile(new File(path))("UTF-8").mkString
+    }
+
+    def loadBinary(assetsDir: String, file: String): IO[Array[Byte]] = IO {
+        val path = assetsDir + File.separator + file
+        Files.readAllBytes(Paths.get(path))
     }
 }
